e6a8fc34e5c02a99903a110e36950b3a6f2b20d5
Amelia Wattenberger <wattenberger@github.com>
2022-05-26 10:51:40 -0700

    init action
    Co-authored-by: Jake Donham <jaked@users.noreply.github.com>

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+const About: NextPage = () => {
+  return (
+    <div>
+      This is a cool site
+      <a href="./">Back</a>
+
+    </div>
+  )
+}
+
+export default About
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -16,6 +15,8 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <a href="about">About page</a>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -61,7 +62,7 @@ const Home: NextPage = () => {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            {/* <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
           </span>
         </a>
       </footer>
